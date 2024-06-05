5f56669675c8150721b659405e74182de60c6489
Fernando Silva <fernando.silva@intel.com>
2024-06-05 15:21:29 -0700

    Initial release - Java sample app with Gramine and Intel(R) SGX

--- sample/java/GlobalErrorHandler.java
+++ sample/java/GlobalErrorHandler.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2024, Intel Corporation. All rights reserved.<BR>
+// SPDX-License-Identifier: BSD-3-Clause
+
 public class GlobalErrorHandler implements Thread.UncaughtExceptionHandler {
 	public void uncaughtException(Thread thead, Throwable ex) {
 		System.out.println("\nUnexpected event while running the application. Exiting...\n");
